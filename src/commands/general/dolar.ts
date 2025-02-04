a7e3fa85b9ce63213fa078bbab30e36ca750c44b
SrFreitass <guilhermefreitasdonascimento67@gmail.com>
2025-02-03 20:52:32 -0400

    feat: add number option on /dolar command

--- src/commands/general/dolar.ts
+++ src/commands/general/dolar.ts
@@ -10,12 +10,20 @@ class DolarCommand implements Command {
         this.data = new SlashCommandBuilder()
             .setName(this.name)
             .setDescription("Veja o preço do dolar com a Virginia Fonseca")
+            .addNumberOption((option) => option.setName("quantidade").setDescription("Quantidade de dolar"));
     }
 
     async execute(interaction: CommandInteraction) {
         try {
             const res = await fetch("https://economia.awesomeapi.com.br/last/USD-BRL");
-            const json = await res.json();    
+            const json = await res.json();   
+            
+            const value = interaction.options.get("quantidade")?.value as number;
+
+            if(value) {
+                return await interaction.reply(`Seguimore do céu, R$${value} dolar é R$${value * json?.USDBRL?.high}! Tudo culpa do Lula 13 PT ⭐, O AMOR VENCEU, #VOLTA BOLSONARO 💖`);
+            }
+            
             await interaction.reply(`Seguimore do céu, o dolar tá R$${json?.USDBRL?.high}! Tudo culpa do Lula 13 PT ⭐, O AMOR VENCEU, #VOLTA BOLSONARO 💖`)
         } catch (error) {
             await interaction.reply("Fui censurada seguimore, não querem divulgar o dolar")
@@ -24,3 +32,4 @@ class DolarCommand implements Command {
 }
 
 export { DolarCommand };
+
