c7a22bf6755e36bea063bc7e27470a866508b013
SrFreitass <guilhermefreitasdonascimento67@gmail.com>
2025-02-21 00:58:51 -0400

    ref: remove followUp in cafeCommand

--- src/commands/general/cafe.ts
+++ src/commands/general/cafe.ts
@@ -27,7 +27,6 @@ class CafeCommand implements Command {
             const response = `O preço do ☕ CAFÉ ARÁBICA CEPEA/ESALQ ${(price/60).toFixed(2)}kg, ${price}/60kg, tá caro né? Só não comprar que o preço abaixa! 13 ⭐ \n O preço do ☕ CAFÉ ROBUSTA CEPEA/ESALQ ${(price2/60).toFixed(2)}kg, ${price2}/60kg, tá caro né? Só não comprar que o preço abaixa! 13 ⭐`;
 
             await interaction.reply(response);
-            await interaction.followUp("O preço do café é atualizado diariamente, para mais informações acesse: https://www.cepea.esalq.usp.br/br/indicador/cafe.aspx \n https://tenor.com/view/elei%C3%A7%C3%B5es2022-debate-band-lula-deus-me-de-for%C3%A7as-meu-pai-amado-gif-26586715");
         } catch (error) {
             await interaction.reply("Fui censurada seguimore, não querem divulgar o preço do café")
         }
