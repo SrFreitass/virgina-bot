ec49dc7e7dfdd41bdb4654affff8a12f52069c11
SrFreitass <guilhermefreitasdonascimento67@gmail.com>
2025-06-23 01:15:25 -0400

    fix: http server and dynamic import commands

--- src/main.ts
+++ src/main.ts
@@ -1,43 +1,40 @@
-import { Events } from 'discord.js';
-import 'dotenv/config';
-import { createServer } from 'node:http2';
-import { client } from './client';
-import './config/refresh-commands';
-import { refreshCommands } from './config/refresh-commands';
-import { dynamicImportCommands } from './utils/dynamic-import-commands';
+import { Events } from "discord.js";
+import "dotenv/config";
+import { createServer } from "node:http";
+import { client } from "./client";
+import "./config/refresh-commands";
+import { refreshCommands } from "./config/refresh-commands";
+import { dynamicImportCommands } from "./utils/dynamic-import-commands";
 
 const main = async () => {
-    const commandsMap = new Map();
-    const commands = await dynamicImportCommands();
-    
-    await refreshCommands(commands);
-    
-    commands.forEach(command => {
-        commandsMap.set(command.name, command);
-    });
-    
-    client.on(Events.ClientReady, () => {
-        console.log("Virginia fonseca está online!")
-    });
-    
-    
-    client.on(Events.InteractionCreate, async (interaction) => {
-        console.log(interaction)
-    
-        if(!interaction.isCommand()) return;
-    
-    
-        await commandsMap.get(interaction.commandName)?.execute(interaction);
-    });
-    
-    client.login(process?.env?.DISCORD_BOT_TOKEN);
-}
+  const commandsMap = new Map();
+  const commands = await dynamicImportCommands();
+
+  await refreshCommands(commands);
+
+  commands.forEach((command) => {
+    commandsMap.set(command.name, command);
+  });
+
+  client.on(Events.ClientReady, () => {
+    console.log("Virginia fonseca está online!");
+  });
+
+  client.on(Events.InteractionCreate, async (interaction) => {
+    console.log(interaction);
+
+    if (!interaction.isCommand()) return;
+
+    await commandsMap.get(interaction.commandName)?.execute(interaction);
+  });
+
+  client.login(process?.env?.DISCORD_BOT_TOKEN);
+};
 
 createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Server is running');
-}
-).listen(3000, '0.0.0.0', () => {
-    console.log('HTTP/2 server is running on port 3000');
-    main();
-})
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Server is running");
+}).listen(8080, () => {
+  console.log("HTTP server is running on port 8080");
+  main();
+});
--- src/utils/dynamic-import-commands.ts
+++ src/utils/dynamic-import-commands.ts
@@ -1,35 +1,37 @@
-import { readdirSync } from 'node:fs';
-import * as path from 'node:path';
-import { Command } from '../types/ICommand';
+import { readdirSync } from "node:fs";
+import * as path from "node:path";
+import { Command } from "../types/ICommand";
 
 const dynamicImportCommands = async () => {
-    const commands: Command[] = [];
+  const commands: Command[] = [];
 
-    const commandsPath = path.resolve(process.cwd() + `/${process?.env?.DEVELOPMENT === "1" ? "src" : "dist" }/commands`);
-    
-    const directories = readdirSync(commandsPath)
-    
-    for(const dir of directories) {
-        console.log(dir);
-    
-        if(dir.includes('.')) continue;
-            
-        const commandsFiles = readdirSync(`${commandsPath}/${dir}`)
-    
-        for(const file of commandsFiles) {
-            console.log(file, "teste")
-    
-            
-            console.log(`${commandsPath}/${dir}/${file}`);
-            
-            const commandImport = await import(`${commandsPath}/${dir}/${file}`);
+  const commandsPath = path.resolve(
+    __dirname +
+      `/../${process?.env?.IS_DEV_ENV === "1" ? "" : "dist"}/commands`,
+  );
 
-            const command = new commandImport[Object.keys(commandImport)[0]]();
-            commands.push(command);   
-        }
+  const directories = readdirSync(commandsPath);
+
+  for (const dir of directories) {
+    console.log(dir);
+
+    if (dir.includes(".")) continue;
+
+    const commandsFiles = readdirSync(`${commandsPath}/${dir}`);
+
+    for (const file of commandsFiles) {
+      console.log(file, "teste");
+
+      console.log(`${commandsPath}/${dir}/${file}`);
+
+      const commandImport = await import(`${commandsPath}/${dir}/${file}`);
+
+      const command = new commandImport[Object.keys(commandImport)[0]]();
+      commands.push(command);
     }
+  }
 
-    return commands;
-}
+  return commands;
+};
 
 export { dynamicImportCommands };
