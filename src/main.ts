afd295001648acebbda3dad7fc5e9bacebe8ffde
SrFreitass <guilhermefreitasdonascimento67@gmail.com>
2025-06-23 01:30:24 -0400

    fix: status code

--- src/main.ts
+++ src/main.ts
@@ -32,7 +32,7 @@ const main = async () => {
 };
 
 createServer((req, res) => {
-  res.writeHead(204, { "Content-Type": "text/plain" });
+  res.writeHead(200, { "Content-Type": "text/plain" });
   res.end("Server is running");
 }).listen(process.env.PORT || 8080, () => {
   console.log("HTTP server is running on port 8080");
